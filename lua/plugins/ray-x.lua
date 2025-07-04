fdf1d17a0a555f8eff7ee1290ccd62bdb78d1b26
Belunin Dmitrii <dbelunin@ozon.ru>
2025-07-05 02:35:48 +0300

    lazy-lock.json added to gitignore; conflicts resolved

--- lua/plugins/ray-x.lua
+++ lua/plugins/ray-x.lua
@@ -1,11 +1,7 @@
 return {
   "ray-x/go.nvim",
   dependencies = { "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
-<<<<<<< HEAD
-  config = function()
-=======
     config = function()
->>>>>>> e33f9a9 (actual setup)
     require("go").setup()
     local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
     vim.api.nvim_create_autocmd("BufWritePre", {
@@ -15,14 +11,9 @@ return {
       end,
       group = format_sync_grp,
     })
-<<<<<<< HEAD
-  end,
-  ft = { "go", "gomod" },
-=======
     end,
     event = {"CmdlineEnter"},
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()'
->>>>>>> e33f9a9 (actual setup)
 }
 
