67f2175196a26e097d2cae6cf3823eadc83450a1
Belunin Dmitrii <dbelunin@ozon.ru>
2025-07-05 02:17:15 +0300

    actual setup

--- lua/plugins/ray-x.lua
+++ lua/plugins/ray-x.lua
@@ -1,7 +1,11 @@
 return {
   "ray-x/go.nvim",
   dependencies = { "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
+<<<<<<< HEAD
   config = function()
+=======
+    config = function()
+>>>>>>> e33f9a9 (actual setup)
     require("go").setup()
     local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
     vim.api.nvim_create_autocmd("BufWritePre", {
@@ -11,7 +15,14 @@ return {
       end,
       group = format_sync_grp,
     })
+<<<<<<< HEAD
   end,
   ft = { "go", "gomod" },
+=======
+    end,
+    event = {"CmdlineEnter"},
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()'
+>>>>>>> e33f9a9 (actual setup)
 }
 
