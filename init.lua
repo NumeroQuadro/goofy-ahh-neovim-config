d5e38d81df03ad6459f7d7a5e8c7eac51d9fdf1b
numero quadro <dimabelunin7@gmail.com>
2025-07-10 19:32:57 +0300

    multiple config support added

--- init.lua
+++ init.lua
@@ -1,18 +1,27 @@
+-- To switch configs, change the name of the string in the line below.
+local active_config = "goofy_ahh_config"
+
+-- Standard lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
-local opts = {}
 
-vim.o.number = true
+-- Load the options for the active config
+require("configs." .. active_config .. ".vim-options")
 
-require("vim-options")
-require("lazy").setup("plugins")
+-- Setup lazy.nvim to load plugins from the active config
+require("lazy").setup({
+  spec = "configs." .. active_config .. ".plugins",
+  change_detection = {
+    notify = false,
+  },
+})
