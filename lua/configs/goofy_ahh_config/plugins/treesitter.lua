cb90ec3ce71954023826eea50fc37e43723112d0
numero quadro <dimabelunin7@gmail.com>
2025-07-10 19:57:05 +0300

    treesitter text objects added (new keyboard shortcuts now available)

--- lua/configs/goofy_ahh_config/plugins/treesitter.lua
+++ lua/configs/goofy_ahh_config/plugins/treesitter.lua
@@ -22,6 +22,43 @@ return {
             },
             highlight = { enable = true },
             indent = { enable = true },
+
+            textobjects = {
+              select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj
+                keymaps = {
+                  -- You can use the capture groups defined in textobjects.scm
+                  ["af"] = "@function.outer",
+                  ["if"] = "@function.inner",
+                  ["ac"] = "@class.outer",
+                  ["ic"] = "@class.inner",
+                  ["ia"] = "@parameter.inner",
+                  ["aa"] = "@parameter.outer",
+                },
+              },
+              move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                  ["]f"] = "@function.outer",
+                  ["]c"] = "@class.outer",
+                },
+                goto_previous_start = {
+                  ["[f"] = "@function.outer",
+                  ["[c"] = "@class.outer",
+                },
+              },
+              swap = {
+                enable = true,
+                swap_next = {
+                  ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                  ["<leader>A"] = "@parameter.inner",
+                },
+              },
+            },
         })
         require("treesitter-context").setup({
             enable = true, -- Enable this plugin
