0eed409cf7dfed80749c87afa26250c87e949ed0
numero quadro <dimabelunin7@gmail.com>
2025-07-12 19:31:33 +0300

    j and k now moves by visible lines on the screen

--- lua/configs/goofy_ahh_config/vim-options.lua
+++ lua/configs/goofy_ahh_config/vim-options.lua
@@ -3,12 +3,16 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.opt.scrolloff = 4
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.cmd("set wrap")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
 
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
