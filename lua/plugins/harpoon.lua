2548b803d296e8c0dbd59a772a6abfa85d9a1c3f
numero quadro <dimabelunin7@gmail.com>
2025-07-20 23:04:45 +0300

    feat: add harpoon plugin for file marking
    Installs  to allow for quick bookmarking
    and navigation of files.
    
    Configuration includes project-specific mark lists and keymaps
    for adding, removing, and navigating marks.

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,21 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local harpoon = require("harpoon")
+
+        -- Harpoon 2 automatically handles project-specific lists.
+        -- No extra setup is needed for that core feature.
+
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
+        vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end, { desc = "Harpoon: Remove file" })
+        vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle quick menu" })
+
+        -- Keymaps to jump to specific marks
+        vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Harpoon: Go to mark 1" })
+        vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Harpoon: Go to mark 2" })
+        vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Harpoon: Go to mark 3" })
+        vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Harpoon: Go to mark 4" })
+    end,
+}
