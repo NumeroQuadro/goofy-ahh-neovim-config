9bc1779c6ada17a9b36228685abfe2ca03100242
numero quadro <dimabelunin7@gmail.com>
2025-08-22 20:18:33 +0300

    telescope: add floating file browser with preview; map <leader>fe to project root; add '-' to open current buffer directory; remove <leader>fE

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-ui-select.nvim", "folke/todo-comments.nvim" },
+        dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-ui-select.nvim", "folke/todo-comments.nvim", "nvim-telescope/telescope-file-browser.nvim" },
         config = function()
             local telescope = require("telescope")
             local builtin = require("telescope.builtin")
@@ -124,11 +124,17 @@ return {
                     -- ["ui-select"] = {
                     --     require("telescope.themes").get_dropdown {}
                     -- }
+                    file_browser = {
+                        hijack_netrw = false,
+                        grouped = true,
+                        hidden = true,
+                    }
                 }
             })
 
             -- telescope.load_extension("ui-select")
             telescope.load_extension("todo-comments")
+            telescope.load_extension("file_browser")
 
             vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "Find files" })
             -- Enhanced diagnostics viewing
@@ -158,6 +164,49 @@ return {
             vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = "Git status" })
             vim.keymap.set('n', '<leader>ft', "<cmd>Telescope todo-comments<cr>", { desc = "Find todos" })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find Buffers" })
+
+            -- Floating file browser with preview at project root
+            vim.keymap.set('n', '<leader>fe', function()
+                require('telescope').extensions.file_browser.file_browser({
+                    path = vim.loop.cwd(),
+                    cwd = vim.loop.cwd(),
+                    respect_gitignore = false,
+                    hidden = true,
+                    grouped = true,
+                    previewer = true,
+                    initial_mode = "normal",
+                    layout_strategy = 'vertical',
+                    layout_config = {
+                        width = 0.95,
+                        height = 0.95,
+                        preview_cutoff = 0,
+                        preview_height = 0.5,
+                    },
+                })
+            end, { desc = "File browser (project root)" })
+
+            -- Quickly open file browser at current buffer's directory
+            vim.keymap.set('n', '-', function()
+                local fb = require('telescope').extensions.file_browser
+                local here = vim.fn.expand("%:p:h")
+                fb.file_browser({
+                    path = here,
+                    cwd = here,
+                    select_buffer = true,
+                    respect_gitignore = false,
+                    hidden = true,
+                    grouped = true,
+                    previewer = true,
+                    initial_mode = "normal",
+                    layout_strategy = 'vertical',
+                    layout_config = {
+                        width = 0.95,
+                        height = 0.95,
+                        preview_cutoff = 0,
+                        preview_height = 0.5,
+                    },
+                })
+            end, { desc = "File browser (current file dir)" })
 
             vim.keymap.set('n', '<leader>ff', function()
                 builtin.live_grep({
