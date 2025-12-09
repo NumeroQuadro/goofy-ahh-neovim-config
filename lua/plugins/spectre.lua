bdd7696efb9081eeaed2aca93e0ca43180fd7fdf
numero quadro <dimabelunin7@gmail.com>
2025-12-09 12:32:56 +0300

    spectre (find&replace) plugin added

---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "windwp/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "Spectre" },
+    keys = {
+      { "<leader>sp", function() require('spectre').open() end,            desc = "Spectre: Project find/replace" },
+      { "<leader>sf", function() require('spectre').open_file_search() end,  desc = "Spectre: File find/replace" },
+      { "<leader>sw", function() require('spectre').open_visual({ select_word = true }) end, mode = {"n","x"}, desc = "Spectre: Search word" },
+      { "<leader>sr", function() require('spectre').toggle() end,            desc = "Spectre: Toggle panel" },
+    },
+    config = function()
+      local ok, spectre = pcall(require, 'spectre')
+      if not ok then return end
+
+      spectre.setup({
+        live_update = true,
+        highlight = { ui = "String", search = "IncSearch", replace = "DiffChange" },
+        mapping = {
+          -- Select entries by file or by line, then run replace only for selected
+          ["toggle_file"] = { map = "<CR>", cmd = require('spectre.actions').toggle_file, desc = "Toggle file selection" },
+          ["toggle_line"] = { map = "<Tab>", cmd = require('spectre.actions').toggle_line, desc = "Toggle line selection" },
+          ["run_replace"] = { map = "r", cmd = require('spectre.actions').run_replace, desc = "Apply to selected" },
+          ["run_current_replace"] = { map = "R", cmd = require('spectre.actions').run_current_replace, desc = "Apply current file" },
+          ["show_options"] = { map = "o", cmd = require('spectre.actions').show_options, desc = "Show options" },
+          ["enter_file"] = { map = "gf", cmd = require('spectre.actions').enter_file, desc = "Open file under cursor" },
+          ["send_to_qf"] = { map = "Q", cmd = require('spectre.actions').send_to_qf, desc = "Send results to quickfix" },
+          ["replace_cmd"] = { map = "c", cmd = require('spectre.actions').replace_cmd, desc = "Input replace (sed)" },
+        },
+      })
+    end,
+  },
+}
+
